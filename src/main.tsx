2d9f1ebd4829340c9863e5865222a44b2a84417a
yasg-uru <yashpawar12122004@gmail.com>
2024-09-15 12:20:20 +0530

    created statemanagement setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Provider } from "react-redux";
+import { store } from "./state-manager/store.ts";
 createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </ThemeProvider>
+  </Provider>
 );
