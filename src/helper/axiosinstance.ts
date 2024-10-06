4b15da8f5772f25566584063a52907fd4f6d1514
yasg-uru <yashpawar12122004@gmail.com>
2024-10-06 23:34:12 +0530

    changed the base url

--- src/helper/axiosinstance.ts
+++ src/helper/axiosinstance.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-const axiosInstance=axios.create({
-    baseURL:"https://trendshpere-backend-2.onrender.com",
-    // baseURL:"http://localhost:8000",
-
-})
+const axiosInstance = axios.create({
+  baseURL: "https://trendshpere-backend-2.onrender.com",
+  // baseURL:"http://localhost:8000",
+});
 export default axiosInstance;
