d81e76146265b86383fb3d5bc75154d41fe661f4
yasg-uru <yashpawar12122004@gmail.com>
2025-01-01 14:23:49 +0530

    corrected some things for the deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -78,6 +79,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
 
